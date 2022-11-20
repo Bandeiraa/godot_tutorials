7cdf9ba5dc6374a264cac34e098da2f1d2b65207
unknown <fcodavibandeira@edu.unifor.br>
2022-11-20 12:17:05 -0300

    new repo (godot 4.0 shaders) - outline and flash effect shader scene sample

---
+++ godot_shaders/scripts/outline_2d.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+@onready var animation: AnimationPlayer = get_node("Animation")
+
+var on_hit: bool = false
+
+func _process(_delta: float) -> void:
+	hit()
+	toggle_outline()
+	
+	
+func hit() -> void:
+	if Input.is_action_just_pressed("ui_select") and not on_hit:
+		animation.play("hit")
+		on_hit = true
+		
+		
+func toggle_outline() -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		material.set_shader_parameter(
+			"is_outline_active",
+			not material.get_shader_parameter("is_outline_active")
+		)
+		
+		
+func on_animation_finished(anim_name: String) -> void:
+	if anim_name == "hit":
+		on_hit = false
+		animation.play("idle")
+		material.set_shader_parameter("is_on_hit", false)
